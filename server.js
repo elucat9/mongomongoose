8d8fb4a458ee657b93255ed420999492a05549b3
elucat9 <elucat9@gmail.com>
2019-03-23 14:26:12 -0700

    blah

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ var db = mongojs(databaseUrl, collections);
 //CONFIGURE MONGOOSE
 var mongoose = require('mongoose');
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI);
 
 
 //SCRAPE WALL STREET JOURNAL WEBSITE
