951f6589457fb69e83d2bacb5537e33e22e50337
elucat9 <elucat9@gmail.com>
2019-03-23 12:32:18 -0700

    heroku

--- scraper.js
+++ scraper.js
@@ -38,18 +38,21 @@ var axios = require("axios");
 var app = express();
 var path = require('path');
 
-//CONFIGURE DB AND MONGO
+//CONFIGURE DB AND MONGOJS
 var databaseUrl = "scraper";
-var collections = ["results"];
+var collections = ["results", "comment"];
 var db = mongojs(databaseUrl, collections);
+
+//CONFIGURE MONGOOSE
 var mongoose = require('mongoose');
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://<dbuser>:<dbpassword>@ds235860.mlab.com:35860/heroku_xxrkkj7s";
-mongoose.connect(MONGODB_URI);
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true});
 
 //SCRAPE WALL STREET JOURNAL WEBSITE
   axios.get("https://www.wsj.com/").then(function (response) {
     var $ = cheerio.load(response.data);
     var results = [];
+    
 
     $("a.wsj-headline-link").each(function (i, element) {
         var Headline = $(element).text();
@@ -93,6 +96,25 @@ app.get("/scrape", function(req, res) {
   });
 
 })
+
+//POST COMMENT 
+// SAVE FORM SUBMISSION
+app.post("/submit", function(req, res) {
+  console.log(req.body);
+  // Insert the note into the notes collection
+  db.comment.insert(req.body, function(error, saved) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    else {
+      // Otherwise, send the note back to the browser
+      // This will fire off the success function of the ajax request
+      res.send(saved);
+    }
+  });
+});
+
  
 
 
